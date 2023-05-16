a5cd18d40a63ad5f97b03aaf1a2689db5cec78c9
vineethayasa <srinivas@DESKTOP-Q5F6FJ7>
2023-05-16 17:59:37 +0530

    created user model

---
+++ app.js
@@ -0,0 +1,7 @@
+const { request, response } = require('express')
+const express = require('express')
+const app = express()
+
+console.log("Abcd")
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("abc")
+const app = require("./app");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
+});
