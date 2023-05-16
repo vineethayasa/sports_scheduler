3ad79b194f221f02fe7eb6d764661fb56f1fce13
vineethayasa <srinivas@DESKTOP-Q5F6FJ7>
2023-05-16 18:21:40 +0530

    basic user route for adding a user

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+/* eslint-disable no-unused-vars */
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
-console.log("Abcd")
+const { User } = require('./models')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.get('/',async (request, response) => {
+    response.send("signup")
+})
+
+app.post('/users', async (request, response) => {
+    try {
+      const user = await User.create({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        email: request.body.email,
+        password: request.body.password,
+        role:request.body.role
+      })
+
+    } catch (error) {
+      console.log(error)
+      response.redirect('/')
+    }
+  })
+  
 
 module.exports = app
