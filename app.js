fdeadc47ea70b65786b5fd06183f35c975ee8332
vineethayasa <srinivas@DESKTOP-Q5F6FJ7>
2023-05-16 18:49:22 +0530

    installed ejs and created signin and signup pages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const { request, response } = require('express')
 const express = require('express')
@@ -8,8 +9,20 @@ const { User } = require('./models')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/',async (request, response) => {
-    response.send("signup")
+    response.render("index")
+})
+
+app.get('/signup', (request, response) => {
+    response.render('signup')
+})
+app.get('/login', (request, response) => {
+    response.render('login')
 })
 
 app.post('/users', async (request, response) => {
@@ -21,7 +34,7 @@ app.post('/users', async (request, response) => {
         password: request.body.password,
         role:request.body.role
       })
-
+      return response.json(user)
     } catch (error) {
       console.log(error)
       response.redirect('/')
