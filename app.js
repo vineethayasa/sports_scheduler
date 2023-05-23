a3007a31e32c29ff9e116fc04b78bc82c2c74a5b
vineethayasa <srinivas@DESKTOP-Q5F6FJ7>
2023-05-23 12:07:22 +0530

    signup page is functioning

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 
 const { User } = require('./models')
 
+app.use(express.urlencoded({ extended: false }))
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
@@ -25,21 +27,47 @@ app.get('/login', (request, response) => {
     response.render('login')
 })
 
+app.get('/users', async (request, response) => {
+    try {
+        const users = await User.findAll();
+        return response.send(users);
+      } catch (error) {
+        console.log(error);
+        return response.status(422).json(error);
+      }
+})
+
 app.post('/users', async (request, response) => {
     try {
-      const user = await User.addTodo({
+      const user = await User.create({
         firstName: request.body.firstName,
         lastName: request.body.lastName,
         email: request.body.email,
         password: request.body.password,
         role:request.body.role
       })
-      return response.json(user)
+    return response.redirect('/')
     } catch (error) {
       console.log(error)
-      response.redirect('/')
+      return response.status(201).json({ message: 'error' });
+
     }
   })
-  
+// app.post('/users', async (request, response) => {
+//   try {
+//     const user = await User.addUser({
+//       firstName: request.body.firstName,
+//       lastName: request.body.lastName,
+//       email: request.body.email,
+//       password: request.body.password,
+//       role: request.body.role
+//     });
+//     return response.status(201).json({ message: 'User created successfully' });
+//   } catch (error) {
+//     console.log(error);
+//     return response.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
 
 module.exports = app
--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-    static addTodo ({ firstName,lastName,email,password,role }) {
+    static addUser ({ firstName,lastName,email,password,role }) {
       return this.create({ firstName,lastName,email,password,role })
     }
   }
